a632886d74af5c936472a1034c151c3cec672b31
joaovitor <joaovfp2002@gmail.com>
2024-10-03 01:06:17 -0300

    create bool compiler class

--- src/BoolCompiler.java
+++ src/BoolCompiler.java
@@ -1,7 +1,28 @@
+import java.io.*;
+
 public class BoolCompiler {
 
     public static void main(String[] args) {
 
-        System.out.println("Teste git");
+        System.out.println("From " + args[ 0] + " to " + args[1]);
+
+        try {
+
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
+
+            String line;
+            while((line = reader.readLine()) != null) {
+
+                writer.write(line + "\n");
+            }
+
+            writer.close();
+            reader.close();
+
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+        }
     }
 }
