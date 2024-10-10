b26cddbf969e2f6bcb7d157fc96b6dae6b29a6b2
joaovitor <joaovfp2002@gmail.com>
2024-10-09 21:19:22 -0300

    create Translator class

--- src/BoolCompiler.java
+++ src/BoolCompiler.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BoolCompiler {
 
@@ -10,16 +13,50 @@ public class BoolCompiler {
 
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
-            Translator translator = new Translator();
+            Pattern begin = Pattern.compile("begin"); // colocar ^ após formatar
+            Pattern end = Pattern.compile("end-?"); // colocar ^ após formatar
+            // Matcher matcher;
+
+            ArrayList<String> lines = new ArrayList<>();
+            ArrayList<String> bodyLines = new ArrayList<>();
 
             String line;
             while((line = reader.readLine()) != null) {
 
-                writer.write(translator.translate(line));
+                line = Useful.format(line);
+
+                lines.add(line);
+
+                // matcher = begin.matcher(line);
+
+                if(begin.matcher(line).find()) {
+
+                    line = reader.readLine();
+                    line = Useful.format(line);
+
+                    while(!end.matcher(line).find()) {
+
+                        bodyLines.add(line);
+
+                        line = reader.readLine();
+                        line = Useful.format(line);
+                    }
+
+                    Useful.translateBody(bodyLines, lines);
+
+                    line = Useful.format(line);
+                    lines.add(line);
+                }
+            }
+
+            for (String s : lines) {
+
+                writer.write(s + "\n");
             }
 
             writer.close();
             reader.close();
+
 
         } catch (IOException e) {
 
---
+++ src/Useful.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Useful {
+
+    public static void translateBody (ArrayList<String> bodyLines, ArrayList<String> lines) {
+
+        for (String line : bodyLines) {
+
+            lines.add("-> " + line + " <-");
+        }
+    }
+
+    public static String format (String text) {
+
+        return text;
+    }
+}
