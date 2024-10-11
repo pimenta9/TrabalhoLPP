77a94efd35108ac07d20bd6b3e0494ecc6af7615
joaovitor <joaovfp2002@gmail.com>
2024-10-10 23:04:41 -0300

    implementado: return e method-call

--- src/MethodCall.java
+++ src/MethodCall.java
@@ -1,9 +1,38 @@
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MethodCall {
 
     public static void process (String line, ArrayList<String> lines) {
 
-        lines.add(line + " (METHOD-CALL)");
+        // line = obj.method(a, b)
+
+        // se necessário : formatar com line = line.replaceAll(" ", "");
+
+        // DÚVIDA.: pode haver mais de um espaço depois da virgula q separa os params?
+        Pattern pattern = Pattern.compile("\\(([a-zA-z]+)|, ([a-zA-z]+)");
+        Matcher matcher = pattern.matcher(line);
+
+        while (matcher.find()) {
+
+            if (matcher.group(1) != null)
+                lines.add("load " + matcher.group(1));
+
+            if (matcher.group(2) != null)
+                lines.add("load " + matcher.group(2));
+        }
+
+        pattern = Pattern.compile("([a-zA-z]+).");
+        matcher = pattern.matcher(line);
+        matcher.find();
+
+        lines.add("load " + matcher.group(1));
+
+        pattern = Pattern.compile("\\.([a-zA-z]+)");
+        matcher = pattern.matcher(line);
+        matcher.find();
+
+        lines.add("call " + matcher.group(1));
     }
 }
--- src/Return.java
+++ src/Return.java
@@ -1,9 +1,19 @@
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Return {
 
     public static void process (String line, ArrayList<String> lines) {
 
-        lines.add(line + " (RETURN)");
+        // line = "return x"
+
+        Pattern pattern = Pattern.compile(" [a-zA-z]+");
+        Matcher matcher = pattern.matcher(line);
+
+        matcher.find();
+
+        lines.add("load" + matcher.group());
+        lines.add("ret");
     }
 }
