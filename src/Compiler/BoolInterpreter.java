9784177895bba0528b91877537b825935983972f
Gabrielle Siqueira <114371800+gabisml@users.noreply.github.com>
2024-10-24 17:30:16 -0300

    Update BoolInterpreter.java

--- src/Compiler/BoolInterpreter.java
+++ src/Compiler/BoolInterpreter.java
@@ -1,1 +1,29 @@
+import java.io.*;
+import java.util.*;
 
+public class BoolInterpreter {
+
+    // Instância de Classificacoes que contém a lógica de execução
+    private Classificacoes classificacoes = new Classificacoes();
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            return;
+        }
+        BoolInterpreter interpreter = new BoolInterpreter();
+        interpreter.run(args[0]);
+    }
+
+    // lendo o compilado
+    private void run(String inputFile) {
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] instruction = line.trim().split(" ");
+                classificacoes.execute(instruction);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
